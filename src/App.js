30bcdfae461b46fe852d01b8630a47770051e55b
OOCLDM\LEEJO <john.lee@oocl.com>
2021-12-13 18:06:54 +0800

    [TodoList][john] feat: write TodoGenerator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import TodoList from './components/TodoList';
+import TodoGenerator from './components/TodoGenerator';
 
 function App() {
   return (
     <div className="App">
-      <TodoList></TodoList>
+      {/* <TodoList></TodoList> */}
+      <TodoGenerator></TodoGenerator>
     </div>
   );
 }
--- src/components/TodoGenerator.js
+++ src/components/TodoGenerator.js
@@ -1,6 +1,19 @@
-function TodoGenerator(){
+import { useState } from "react";
+
+function TodoGenerator(props){
+
+    const[todo,setTodo] = useState("");
+
+    function add(){
+        setTodo(todo)
+        props.updateTodo(todo);
+    }
+    
     return(
-        <></>
+        <>
+            <input value={todo} type="String"></input>
+            <button onClick={add}>add</button>
+        </>
     )
 }
 
